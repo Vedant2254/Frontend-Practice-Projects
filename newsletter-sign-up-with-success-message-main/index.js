3dabe168d3580eee2cc4a738328473feb0264f53
Vedant Patil <pvedant2002@gmail.com>
2023-08-03 13:53:56 +0530

    newsletter-sign-up... only button shadows remain

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,1 +1,37 @@
-console.log("I am linked to html");
+const emailInputError = document.querySelector(".email-input-error");
+const emailInput = document.querySelector(".email-input");
+const emailForm = document.querySelector(".email-form");
+const emailSubmitBtn = document.querySelector(".email-submit-btn");
+const userEmail = document.querySelector(".user-email");
+const submitPage = document.querySelector(".page1");
+const thankyouPage = document.querySelector(".page2");
+const dismissBtn = document.querySelector(".dismiss-btn");
+
+emailForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  if (emailInput.checkValidity()) {
+    const { value } = emailInput;
+    userEmail.innerText = value;
+    submitPage.classList.add("hidden");
+    thankyouPage.classList.remove("hidden");
+  } else {
+    emailInput.classList.add("input-error");
+    emailInputError.classList.remove("hidden");
+  }
+});
+
+emailInput.addEventListener("input", () => {
+  if (emailInput.value && !emailInput.checkValidity()) {
+    emailInput.classList.add("input-error");
+    emailInputError.classList.remove("hidden");
+  } else {
+    emailInput.classList.remove("input-error");
+    emailInputError.classList.add("hidden");
+  }
+});
+
+dismissBtn.addEventListener("click", () => {
+  submitPage.classList.remove("hidden");
+  thankyouPage.classList.add("hidden");
+});
